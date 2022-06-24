b8780e3ac010999617501fb31012b51e2dc0e0ce
Gamoraes20 <Ka_bmel@outlook.com>
2022-06-24 12:56:52 -0300

    addex3ne

---
+++ angular_backend-MEAN3/src/app/livros/livro.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { from, Subject } from "rxjs";
+import { Livro } from "./livro.model"
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class LivroService{
+  private livros: Livro[] = [];
+  private listaLivrosAtualizada = new Subject<Livro[]>();
+
+  constructor (private httpClient: HttpClient){
+  }
+
+  getLivros(): void {
+    this.httpClient.get <{mensagem: string, livros:
+      Livro[]}>('http://localhost:3000/api/livros').subscribe(
+            (dados) => {
+              this.livros = dados.livros;
+              this.listaLivrosAtualizada.next([...this.livros]);
+            }
+          )
+  }
+
+  adicionarLivro(id: number, titulo: string, autor: string, numero_paginas: number) : void{
+    const livro: Livro = {
+      id: id,
+      titulo: titulo,
+      autor: autor,
+      numero_paginas: numero_paginas
+    };
+    this.httpClient.post<{mensagem: string}> ('http://localhost:3000/api/livros',
+      livro).subscribe(
+            (dados) => {
+              console.log(dados.mensagem);
+              this.livros.push(livro);
+              this.listaLivrosAtualizada.next([...this.livros]);
+            }
+          )
+  }
+
+  getListaDeLivrosAtualizadaObservable(){
+    return this.listaLivrosAtualizada.asObservable()
+  }
+}
